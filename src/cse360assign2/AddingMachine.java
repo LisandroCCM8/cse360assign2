2a7456119e325816db6cc893c5dd1308cacfeb0f
Lisandro <cuestaslizandro@gmail.com>
2019-10-08 16:25:30 -0700

    Initial

--- src/cse360assign2/AddingMachine.java
+++ src/cse360assign2/AddingMachine.java
@@ -1,26 +1,39 @@
+/*
+Name: Lisandro Cuestas
+Assignment #: 2
+Class ID: Friday 9:40 - 10:30
+Description: Create a calculator adds or subtracts and prints out the order in which the operations
+             were performed.
+*/
 package cse360assign2;
 
-public class AddingMachine {
+public class AddingMachine
+{
 
     private int total;
 
-    public AddingMachine () {
+    public AddingMachine ()
+    {
         total = 0;  // not needed - included for clarity
     }
 
-    public int getTotal () {
+    public int getTotal ()
+    {
         return 0;
     }
 
-    public void add (int value) {
+    public void add (int value)
+    {
 
     }
 
-    public void subtract (int value) {
+    public void subtract (int value)
+    {
 
     }
 
-    public String toString () {
+    public String toString ()
+    {
         return "";
     }
 
